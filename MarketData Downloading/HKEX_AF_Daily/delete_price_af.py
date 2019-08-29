601a797c908e37b85fdc956cd44634ccda05194b
chordou <49122703+chordou@users.noreply.github.com>
2019-08-29 18:01:06 +0800

    Add files via upload

--- MarketData Downloading/HKEX_AF_Daily/delete_price_af.py
+++ MarketData Downloading/HKEX_AF_Daily/delete_price_af.py
@@ -3,14 +3,17 @@ ip = '10.63.16.76'
 
 #Attention:
 # make sure collection_name and db_name is what you need
+# keep feature: 'price_af_star'
+
 db_name = 'MarketDataUpdate'
-collection_name = 'HKEX_ADJ'
+collection_name = 'HKEX_AF_Daily'
 
 mng_client = pymongo.MongoClient(ip, 27017)
 mng_db = mng_client[db_name]
 
 db_cm = mng_db[collection_name]
-myquery = { 'adjust': 'all'}
-#myquery = { }
-db_cm.delete_many(myquery)
+myquery = { 'feature' : 'price_af'}
+print('deleting...')
 
+db_cm.delete_many(myquery)
+print('deleting finished')
